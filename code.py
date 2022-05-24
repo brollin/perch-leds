f37d5a2a42b0baf1325ea8d4184381dab66253f9
Ben Rollin <ben.rollin@clever.com>
2022-05-23 19:28:40 -0700

    smoother tron

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ from bladerunner import Bladerunner
 from fire2012 import Fire2012
 from fadeout import Fadeout
 from pixelconfig import PixelConfig
+from ripple import Ripple
 from tron import Tron
 
 pixel_config = PixelConfig()
@@ -15,6 +16,7 @@ time.sleep(0.5)
 
 # list of patterns to cycle through
 patterns = [
+    # Ripple(pixel_config),
     Tron(pixel_config),
     Bladerunner(pixel_config),
     Fire2012(pixel_config),
--- tron.py
+++ tron.py
@@ -1,34 +1,46 @@
 from random import random
 from pattern import Pattern
+from util import lerp3
 
 TEAL = (19, 241, 242)
-BLUE = (59, 94, 166)
+# BLUE = (59, 94, 166)
 PURPLE = (115, 21, 114)
-MAGENTA = (192, 0, 80)
+# MAGENTA = (192, 0, 80)
 RED = (255, 38, 54)
 YELLOW = (255, 170, 26)
-COLORS = [TEAL, BLUE, PURPLE, MAGENTA, RED, YELLOW]
+COLORS = [TEAL, PURPLE, RED, YELLOW]
 
 class Tron(Pattern):
-    duration = 6000
     fps = 60
 
     def initialize(self):
         super().initialize()
 
-        self.color_index = 0
+        self.color_index = 3
         self.length = 0
         self.direction = -1 if random() > 0.5 else 1
+        self.transition = 0.2 * self.pixel_config.count
 
     def progress(self):
         count = self.pixel_config.count
-        if self.direction == -1:
-            self.pixel_config.pixels[count - self.length - 1] = COLORS[self.color_index]
-        else:
-            self.pixel_config.pixels[self.length] = COLORS[self.color_index]
+        color = COLORS[self.color_index]
+        for i in range(count):
+            if self.direction == 1:
+                last_color = self.pixel_config.pixels[count - 1]
+                if i < self.length - self.transition:
+                    self.pixel_config.pixels[i] = color
+                elif i < self.length:
+                    self.pixel_config.pixels[i] = lerp3(i, self.length - self.transition, self.length, color, last_color)
+            else:
+                last_color = self.pixel_config.pixels[0]
+                if i < self.length - self.transition:
+                    self.pixel_config.pixels[count - i - 1] = color
+                elif i < self.length:
+                    self.pixel_config.pixels[count - i - 1] = lerp3(i, self.length - self.transition, self.length, color, last_color)
 
         self.length += 1
-        if self.length >= count:
+        if self.length >= count + self.transition:
             self.length = 0
             self.direction = -1 if random() > 0.5 else 1
-            self.color_index = (self.color_index + 1) % len(COLORS)
+            # select new random color
+            self.color_index = (self.color_index + 1 + round(random() * (len(COLORS) - 2))) % len(COLORS)
